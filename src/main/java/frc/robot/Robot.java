ec901a43454be36cd76e58dd2ddfba06cccf44c0
FRC Team 6032 <56695163+frcteam6032@users.noreply.github.com>
2022-03-12 09:43:27 -0500

    12Mar2022

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,16 +7,27 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.XboxController;
 
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkMaxPIDController;
+
+//import com.revrobotics.RelativeEncoder;
+//import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxLimitSwitch;
+import com.revrobotics.CANSparkMax.IdleMode;
+//import com.revrobotics.RelativeEncoder;
+//import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.cscore.VideoSource.ConnectionStrategy;
 
 /**
  * Before Running:
@@ -24,109 +35,224 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
  * shuffleboard.json that is in the root directory of this example
  */
 
-import edu.wpi.first.cameraserver.CameraServer;
-//import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.cscore.VideoSource.ConnectionStrategy;
-
 
 public class Robot extends TimedRobot {
+  
 // Create motor instances
+
+//private SparkMaxPIDController m_pidController;
 
   private final CANSparkMax m_rightMotor = new CANSparkMax(1, MotorType.kBrushless);
   private final CANSparkMax m_rightFollower = new CANSparkMax(2, MotorType.kBrushless);
   private final CANSparkMax m_leftMotor = new CANSparkMax(3, MotorType.kBrushless);
   private final CANSparkMax m_leftFollower = new CANSparkMax(4, MotorType.kBrushless);
+ private final CANSparkMax m_Lift = new CANSparkMax(5, MotorType.kBrushless); 
+ // Above for fake robot, change 99 to 5
+ private final SparkMaxLimitSwitch m_ForwardLimit =  m_Lift.getForwardLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
+ private final SparkMaxLimitSwitch m_ReverseLimit =  m_Lift.getReverseLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
+   
+ private final CANSparkMax m_Climb = new CANSparkMax(7, MotorType.kBrushless);
+ //private final CANSparkMax m_AloneClimb = new CANSparkMax(7, MotorType.kBrushless);
+ private final CANSparkMax m_ClimbFollower = new CANSparkMax(8, MotorType.kBrushless);
+ private final CANSparkMax m_Intake = new CANSparkMax(6, MotorType.kBrushed);
+AddressableLED m_led;
+AddressableLEDBuffer m_ledBuffer;
+ // private RelativeEncoder m_encoder =  m_Lift.getEncoder();
+  //public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+  double counter = 0;
+  //boolean DidRun = false;
+  // true = raised
+  /*//
+ private boolean LiftState = true;
+ private boolean ClimbState = true;
+  private final void StartIntake() {
+   m_Intake.set(0);
+   try {
+    Thread.sleep(1000);
+  } catch (InterruptedException e) {
+   
+    e.printStackTrace();
+  }
+m_Intake.set(-0.3);
+try {
+  Thread.sleep(1000);
+} catch (InterruptedException e) {
+  
+  e.printStackTrace();
+}
+m_Intake.set(0.3);
+  return;
+ }
+ // true = raised ; false = lowered
+ private final void Lift() throws InterruptedException {
+if (LiftState == true) {
+  m_Lift.set(-0.15); 
+  Thread.sleep(2000);
+m_Lift.set(0);
+LiftState = false;}
+else {
+  m_Lift.set(0.15); 
+  Thread.sleep(2000);
+m_Lift.set(0);
+LiftState = true;
+}
+  return;
+ }
+  // Need to define intake and lift motors
 
-  // Create PID instances
+  // true = raised ; false = lowered
+  private final void Climb() throws InterruptedException {
+  if (ClimbState == true) {
+    m_Climb.set(0.1); 
+    Thread.sleep(2000);
+  m_Climb.set(0);
+  ClimbState = false;}
+  else {
+    m_Climb.set(-0.1); 
+    Thread.sleep(2000);
+  m_Climb.set(0);
+  ClimbState = true;
+  }
+    return;
+   }
+*/
 
-  private SparkMaxPIDController m_pidControllerRight;
-  private SparkMaxPIDController m_pidControllerLeft;
+/*// Create PID instances
 
-  private RelativeEncoder m_encoderRight;
-  private RelativeEncoder m_encoderLeft;
+    private SparkMaxPIDController m_pidControllerRight;
+    private SparkMaxPIDController m_pidControllerLeft;
 
-  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, maxVel, minVel, maxAcc, allowedErr;
+    private RelativeEncoder m_encoderRight;
+    private RelativeEncoder m_encoderLeft;
 
-  // Create drive instance
+    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, maxVel, minVel, maxAcc, allowedErr;
+
+*/
+// Create drive instance
 
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
   
-  // Create Xbox Controller instance
+// Create Xbox Controller instance
 
   private final XboxController m_driverController = new XboxController(0);
-  
-  // Define global variables
-  private double startTime;
+  private final XboxController m_driverController2 = new XboxController(1);
+  // Define global variables need within the code
+
+  double startTime = 0;
+  double curTime = 0;
+  double autoTimeElapsed = 0;
   UsbCamera camera;
+
+
+  
+  /**
+  * This function runs when the robot is first started up.  Insert robot initialization code in 
+  * this function
+  */
 
   @Override
 
   public void robotInit() {
+    
+    m_led = new AddressableLED(1);
+m_ledBuffer = new AddressableLEDBuffer(60);
+for(var i = 0; i <  6; i++) {
+m_ledBuffer.setRGB(i, 255, 0, 0);
 
-    /* Invert one side of the drivetrain so that positive voltages
-       result in both sides moving forward. The practice robot's
-       gearbox is constructed such that we have to invert the right side. */
+}
+   // m_pidController = m_Lift.getPIDController();
+    
 
-      m_rightMotor.setInverted(true);
-      m_leftFollower.follow(m_leftMotor);
-      m_rightFollower.follow(m_rightMotor);
+  /* Invert one side of the drivetrain so that positive voltages
+  *  result in both sides moving forward. The practice robot's
+  *  gearbox is constructed such that we have to invert the right side.
+  *  Also set the follow motors to follow the respective lead motors
+  */
 
-      // initialze PID controller and encoder objects
+    m_rightMotor.setInverted(true);
+    m_leftFollower.follow(m_leftMotor);
+    m_rightFollower.follow(m_rightMotor);
 
-      m_pidControllerRight = m_rightMotor.getPIDController();
-      m_encoderRight = m_rightMotor.getEncoder();
+    
 
-      m_pidControllerLeft = m_leftMotor.getPIDController();
-      m_encoderLeft = m_leftMotor.getEncoder();
+    m_ClimbFollower.setInverted(true);
+    
+/*
+    kP = 0.5; 
+    kI = 0;
+    kD = 0.5; 
+    kIz = 0.1; 
+    kFF = 0.1; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
 
-     
-      // PID coefficients
+   
+    
+    m_pidController.setP(kP);
+    m_pidController.setI(kI);
+    m_pidController.setD(kD);
+    m_pidController.setIZone(kIz);
+    m_pidController.setFF(kFF);
+    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+*/
 
-      kP = 5e-5; 
-      kI = 1e-6;
-      kD = 0; 
-      kIz = 0; 
-      kFF = 0.000156; 
-      kMaxOutput = 1; 
-      kMinOutput = -1;
-      maxRPM = 5700;
+    
+  /*// initialze PID controller and encoder objects
 
-     // Smart Motion Coefficients
+    m_pidControllerRight = m_rightMotor.getPIDController();
+    m_encoderRight = m_rightMotor.getEncoder();
 
-     maxVel = 2000; // rpm
-     maxAcc = 1500;
+    m_pidControllerLeft = m_leftMotor.getPIDController();
+    m_encoderLeft = m_leftMotor.getEncoder();
  
-     // set PID coefficients
+  // PID coefficients
 
-     m_pidControllerRight.setP(kP);
-     m_pidControllerRight.setI(kI);
-     m_pidControllerRight.setD(kD);
-     m_pidControllerRight.setIZone(kIz);
-     m_pidControllerRight.setFF(kFF);
-     m_pidControllerRight.setOutputRange(kMinOutput, kMaxOutput);
+    kP = 5e-5; 
+    kI = 1e-6;
+    kD = 0; 
+    kIz = 0; 
+    kFF = 0.000156; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
+    maxRPM = 5700;
 
-     m_pidControllerLeft.setP(kP);
-     m_pidControllerLeft.setI(kI);
-     m_pidControllerLeft.setD(kD);
-     m_pidControllerLeft.setIZone(kIz);
-     m_pidControllerLeft.setFF(kFF);
-     m_pidControllerLeft.setOutputRange(kMinOutput, kMaxOutput);
+  // Smart Motion Coefficients
 
-     /**
-     * Smart Motion coefficients are set on a SparkMaxPIDController object
-     * 
-     * - setSmartMotionMaxVelocity() will limit the velocity in RPM of
-     * the pid controller in Smart Motion mode
-     * - setSmartMotionMinOutputVelocity() will put a lower bound in
-     * RPM of the pid controller in Smart Motion mode
-     * - setSmartMotionMaxAccel() will limit the acceleration in RPM^2
-     * of the pid controller in Smart Motion mode
-     * - setSmartMotionAllowedClosedLoopError() will set the max allowed
-     * error for the pid controller in Smart Motion mode
-     */
+    maxVel = 2000; // rpm
+    maxAcc = 1500;
+ 
+  // set PID coefficients
+  
 
-      int smartMotionSlot = 0;
+    m_pidControllerRight.setP(kP);
+    m_pidControllerRight.setI(kI);
+    m_pidControllerRight.setD(kD);
+    m_pidControllerRight.setIZone(kIz);
+    m_pidControllerRight.setFF(kFF);
+    m_pidControllerRight.setOutputRange(kMinOutput, kMaxOutput);
+
+    m_pidControllerLeft.setP(kP);
+    m_pidControllerLeft.setI(kI);
+    m_pidControllerLeft.setD(kD);
+    m_pidControllerLeft.setIZone(kIz);
+    m_pidControllerLeft.setFF(kFF);
+    m_pidControllerLeft.setOutputRange(kMinOutput, kMaxOutput);*/
+
+  /**
+   * Smart Motion coefficients are set on a SparkMaxPIDController object
+   * 
+   * - setSmartMotionMaxVelocity() will limit the velocity in RPM of
+   *   the pid controller in Smart Motion mode
+   * - setSmartMotionMinOutputVelocity() will put a lower bound in
+   *   RPM of the pid controller in Smart Motion mode
+   * - setSmartMotionMaxAccel() will limit the acceleration in RPM^2
+   *   of the pid controller in Smart Motion mode
+   * - setSmartMotionAllowedClosedLoopError() will set the max allowed
+   *   error for the pid controller in Smart Motion mode
+   */
+
+    /*  int smartMotionSlot = 0;
+
       m_pidControllerRight.setSmartMotionMaxVelocity(maxVel, smartMotionSlot);
       m_pidControllerRight.setSmartMotionMinOutputVelocity(minVel, smartMotionSlot);
       m_pidControllerRight.setSmartMotionMaxAccel(maxAcc, smartMotionSlot);
@@ -137,7 +263,7 @@ public class Robot extends TimedRobot {
       m_pidControllerLeft.setSmartMotionMaxAccel(maxAcc, smartMotionSlot);
       m_pidControllerLeft.setSmartMotionAllowedClosedLoopError(allowedErr, smartMotionSlot);
 
-      // display PID coefficients on SmartDashboard
+    // display PID coefficients on SmartDashboard
 
       SmartDashboard.putNumber("P Gain", kP);
       SmartDashboard.putNumber("I Gain", kI);
@@ -147,7 +273,7 @@ public class Robot extends TimedRobot {
       SmartDashboard.putNumber("Max Output", kMaxOutput);
       SmartDashboard.putNumber("Min Output", kMinOutput);
 
-      // display Smart Motion coefficients
+    // display Smart Motion coefficients
 
       SmartDashboard.putNumber("Max Velocity", maxVel);
       SmartDashboard.putNumber("Min Velocity", minVel);
@@ -156,64 +282,170 @@ public class Robot extends TimedRobot {
       SmartDashboard.putNumber("Set Position", 0);
       SmartDashboard.putNumber("Set Velocity", 0);
   
-      // button to toggle between velocity and smart motion modes
+    // button to toggle between velocity and smart motion modes
 
       SmartDashboard.putBoolean("Mode", true);
+      */
   
-
+      //m_Lift.burnFlash();
+      //Automatic
+      
     // Start Microsoft camera capture
-    
-    camera = CameraServer.startAutomaticCapture(0);
+    m_Lift.burnFlash();
+      camera = CameraServer.startAutomaticCapture(0);
 
-    // Make sure camera is always on
+    // Make sure camera is always on.
 
-    camera.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+      camera.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
 
   }
 
   @Override
 
   public void autonomousInit() {
+ 
     startTime = Timer.getFPGATimestamp();
+    m_leftMotor.setIdleMode(IdleMode.kCoast);
+    m_rightMotor.setIdleMode(IdleMode.kCoast);
+    m_leftFollower.setIdleMode(IdleMode.kCoast);
+    m_rightFollower.setIdleMode(IdleMode.kCoast);
   }
 
   @Override
 
   public void autonomousPeriodic() {
-  double curTime = Timer.getFPGATimestamp();
-double speed = 0.3;
-double time = 1;
-  if (curTime - startTime < time) {
-    m_rightMotor.set(speed);
-    m_leftMotor.set(speed);
-   }
+
+    autoTimeElapsed = Timer.getFPGATimestamp() - startTime;
+ 
+    if (autoTimeElapsed < 2.5) {
+      m_Intake.set(-1);
+    }
+    else if (autoTimeElapsed < 4.5) {
+      m_Intake.set(0);
+      m_rightMotor.set(-0.3);
+      m_leftMotor.set(-0.3);
+      //m_robotDrive.tankDrive(-0.3, -0.3);
+    } 
     else {
-    m_rightMotor.set(0);
-    m_leftMotor.set(0);
-    
-  }
+      m_rightMotor.set(0);
+      m_leftMotor.set(0);
+    } 
   } 
+
 
   @Override
 
   public void teleopInit() {
-
-  } 
-
+//m_Intake.set((.3));
+  m_ClimbFollower.follow(m_Climb, true);
+  m_leftMotor.setIdleMode(IdleMode.kBrake);
+  m_rightMotor.setIdleMode(IdleMode.kBrake);
+  m_leftFollower.setIdleMode(IdleMode.kBrake);
+  m_rightFollower.setIdleMode(IdleMode.kBrake);
+  }
   @Override
-
-  public void teleopPeriodic() { 
   
+  public void 
+  teleopPeriodic() { 
+    /*
+    SmartDashboard.putNumber("Time", counter++);
+    SmartDashboard.putNumber("trigger", m_driverController.getRightTriggerAxis());
+    SmartDashboard.putBoolean("A button", m_driverController.getAButton());
+    SmartDashboard.putBoolean("X button", m_driverController.getXButton());
+    SmartDashboard.putNumber("pov", m_driverController.getPOV());
+    SmartDashboard.putBoolean("FWD Limit", m_ForwardLimit.isPressed());
+    SmartDashboard.putBoolean("REV Limit", m_ReverseLimit.isPressed());
+*/
+
   /* Drive with tank drive or arcade drive using an xbox controller. The chosen driver
      driver will make the final choice. */
 
-   m_robotDrive.tankDrive(-m_driverController.getLeftY(), -m_driverController.getRightY());
-   // m_robotDrive.arcadeDrive(-m_driverController.getRightY(), m_driverController.getRightX());
+   //m_robotDrive.tankDrive(-m_driverController.getLeftY(), -m_driverController.getRightY());
+    m_robotDrive.arcadeDrive(-m_driverController.getRightY(), m_driverController.getRightX(), true);
+   
+    
+    if ( // Positive = Raised
+      m_driverController2.getRightTriggerAxis() > .9
+    ){m_Climb.set(.5);}
+
+    else if ( // Negative = Lowered
+      m_driverController2.getLeftTriggerAxis() > .9
+    ){m_Climb.set(-.5);}
+    else if //Turns off clime when no button is pressed
+    (m_driverController2.getLeftTriggerAxis() == 0)
+    {m_Climb.set(0);}
+
+//lower lift when up arrow is pressed
+  if (m_driverController2.getLeftBumper()) {
+      m_Intake.set(.5);
+    }
+
+    if ( //disable intake
+    m_driverController2.getBButton()
+  ){
+    m_Intake.set(0);
+    m_Climb.set(0);
+  }
+
+    if( //lower lift when down arrow is pressed
+      m_driverController2.getRightBumper()
+    ){
+      m_Intake.set(-1);
+    }
+
+
+   if( //lower lift when pressed
+    m_driverController2.getPOV() == 180){
+      m_Lift.set(-.1);
+    } 
+
+     else if( //higher lift when pressed
+    m_driverController2.getPOV() == 0){
+      m_Lift.set(.25);
+      
+    }
+    else { //Stop lift when no button is pressed
+      m_Lift.set(0);
+    }
+
+  
+
+    /*//  if(
+   m_driverController.getRightBumperPressed() ){
+   ;}
+
+
+
+   if(m_driverController.getAButton())
+   {try 
+     {Lift();} //Controls the lift
+   catch 
+   (InterruptedException e) {
+    e.printStackTrace(); 
+  }
+
+
+
+  if(m_driverController.getXButton())
+  {try 
+    {Climb();} 
+    catch 
+    (InterruptedException e) {
+    
+    e.printStackTrace();
+
+    
+  }
+}
+}
+*/
+
   }
 
   @Override
 
   public void testInit() {
+m_ClimbFollower.restoreFactoryDefaults();
 
   }
   
@@ -221,5 +453,35 @@ double time = 1;
 
   public void testPeriodic() {
 
+  
+    m_robotDrive.arcadeDrive(-m_driverController.getRightY(), m_driverController.getRightX());
+  
+// Below controls Joe
+  if (m_driverController.getRightBumper()){
+  //  SmartDashboard.putNumber("bob", 1);
+    m_ClimbFollower.set(.3); // Positive = Raised
+  } else if (
+    m_driverController.getRightTriggerAxis() > .9)
+   {
+  //  SmartDashboard.putNumber("bob", -1);
+   m_ClimbFollower.set(-.3); // Negative = Lowered
+  } else {
+  m_ClimbFollower.set(0);
+  //SmartDashboard.putNumber("bob", 0);
   }
+  
+// Below controls Dificíl
+  if (m_driverController.getLeftBumper()){
+    m_Climb.set(-.3); // Positive = Raised
+  } else if (
+    m_driverController.getLeftTriggerAxis() > .9
+  ) {
+    m_Climb.set(.3); // Negative = Lowered
+  } else {
+    m_Climb.set(0);
+  }
+
+  }
+
+  
 }
